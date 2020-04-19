368fff48a796db6c83c56e1b0d1dc186cd8c8d83
MViktorinova <61969447+MViktorinova@users.noreply.github.com>
2020-04-19 18:08:40 +0200

    Added comments and corrected the name of the txt

--- testing_text_code.py
+++ testing_text_code.py
@@ -1,9 +1,11 @@
 def new_file(text):
-    with open("testing_test.txt", "w") as text_file:
+    """Creates a new testing file"""
+    with open("testing_text.txt", "w") as text_file:
         text_file.write(text)
 
 
 def main():
+    """Driver function"""
     text = "Hello everyone! What are you doing during quarantine? Are you making face mask?"
     new_file(text)
 
