1b30808f789c11f12a4b6d5583391d20c3e2400e
MViktorinova <61969447+MViktorinova@users.noreply.github.com>
2020-04-21 18:08:39 +0200

    uses dictionary which is saved locally

--- words_transform.py
+++ words_transform.py
@@ -1,76 +1,43 @@
-import requests
-
-
 def get_words():
-    """Gets words from the web and saves them to variable 'words'"""
-    website = "https://www.wordgamedictionary.com/english-word-list/download/english.txt"
-    response = requests.get(website)
-    response.encoding = "utf-8"
-    words = response.text
+    """Saves content of the file dictionary.txt into the variable words"""
+    with open("dictionary.txt") as file:
+        words = ""
+        for line in file:
+            words = words + line
 
-    if response.status_code == 200:
-        return words
-
-    else:
-        print("Could not find the url!")
-        return
-
-
-def save_words(words):
-    """Saves variable 'words' into the new file dictionary.txt"""
-    file = open("dictionary.txt", "w+")
-
-    for char in words:
-        if char == "_":
-            char = " "
-        file.write(str(char))
-    file.close()
-
-    return
+    return words
 
 
 def save_words_with_keys(words):
     """Saves variable 'words' into the new file dictionary_with_keys.json"""
     file = open("dictionary_with_keys.json", "w+")
 
-    order = "0123456789abcdefghijklmnopqrstuvwxyz"
+    order = "abcdefghijklmnopqrstuvwxyz"
     order_index = 0
     string = ""
-    get_smaller_index = False
     first_time = True
     length = 0
 
     file.write('{"%s":{' % order[order_index])
     for index, char in enumerate(words):
-        if get_smaller_index:
-            index -= 1
-            char = words[index]
-            get_smaller_index = False
-
         if (string == "") or (string[0] == order[order_index]):
-            if char == "_":
-                string = string + " "
-
-            elif char == "\r":
-                if " " not in string:
-                    if first_time:
-                        file.write('"%s": %d' % (string, length))
-                        first_time = False
-                    else:
-                        file.write(', "%s": %d' % (string, length))
+            if char == "\n":
+                if first_time:
+                    file.write('"%s": %d' % (string, length))
+                    first_time = False
+                else:
+                    file.write(', "%s": %d' % (string, length))
 
                 length = 0
                 string = ""
 
-            elif (char != "\n") and (char != "\r"):
+            else:
                 string = string + char
-                string = string.lower()
                 length += 1
 
         elif string[0] != order[order_index] and (order_index + 1 < len(order)):
             order_index += 1
             file.write('},\n"%s":{' % order[order_index])
-            get_smaller_index = True
             first_time = True
 
     file.write('}}')
@@ -82,7 +49,6 @@ def save_words_with_keys(words):
 def main():
     """The driver function"""
     words = get_words()
-    save_words(words)
     save_words_with_keys(words)
 
 
